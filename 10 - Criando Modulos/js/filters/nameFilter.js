176e45dd9f50f52b1f26fe9f953ca09845d4e2a8
renanmpn <renanmpn@hotmail.com>
2015-12-07 23:58:19 -0200

    Aula 10 - Criando componentes

---
+++ 10 - Criando Modulos/js/filters/nameFilter.js
@@ -0,0 +1,16 @@
+angular.module("listaTelefonica").filter("name",function(){
+	return function (input){
+		var listaDeNomes = input.split(" ");
+		
+		var listaDeNomesFormatada = listaDeNomes.map(function(nome){
+			if(/(da|de)/.test(nome.toLowerCase())){
+				return nome.toLowerCase();
+			}else{
+				return nome.charAt(0).toUpperCase() + nome.substring(1).toLowerCase();
+			}
+		});
+		
+		return listaDeNomesFormatada.join(" ");
+		
+	}
+});
